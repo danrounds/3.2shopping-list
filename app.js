75371f8d4a8a40ca48675d7b145888d46761ab75
danrounds <dan.rounds@gmail.com>
2016-12-11 01:02:39 -0500

    fundamentally-working version 0.1

--- app.js
+++ app.js
@@ -1,95 +1,110 @@
 'use strict';
 
-// State. Duh.
+// State & state manipulation
 var state = {
-    // items: []
-    items:
-      $('.shopping-item').map(function(){
-          return $(this).text();
-      }).get()
-    // items: $('.shopping-item').map(
-    //     function(){
-    //         if ($(this).hasClass('shopping-item__checked'))
-    //             var value = 'shopping-item__checked';
-    //         else
-    //             value = '';
-    //         var text = this.text();
-    //         return { text : value };
-    //     }).get()
+    //items are of the form { itemName: apples, checked: false }
+    items: $('.shopping-item').map(function(){
+        var checked =  $(this).hasClass('shopping-item__checked');
+        return { itemName: $(this).text(), 'checked': checked };
+    }).get()
 };
 
-var itemTemplate = (
-    // HTML for ITEM we're adding, when we click 'Add item'
-    '<li>' +
-      '<span class="shopping-item">ITEM</span>' +
-      '<div class="shopping-item-controls">' +
-        '<button class="shopping-item-toggle">' +
-          '<span class="button-label">check</span>' +
-        '</button>' +
-        '<button class="shopping-item-delete">' +
-          '<span class="button-label">delete</span>' +
-        '</button>' +
-      '</div>' +
-    '</li>'
-);
+function getItem(state, item) {
+    for (var i in state.items) {
+        if (state.items[i].itemName === item) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 function addItem(state, item) {
-    if (!(state.items.indexOf(item) + 1)) {
-        if (item.length > 0) {
-            state.items.push(item);
+    if (item.length > 0) {
+        if (!(getItem(state, item) + 1)) {
+            state.items.push({ 'itemName':item, 'checked':false });
         }
     }
 }
 
 function removeItem(state, item) {
-    // semi-useless error handling, but let's pretend we
-    // might extend this app
-    var i = state.items.indexOf(item);
+    // Semi-useless error handling, but let's pretend might extend
+    // this app
+    var i = getItem(state, item);
     if (i + 1) {
         state.items.splice(i, 1);
     }
 }
+
+function checkItem(state, item) {
+    var i = getItem(state, item);
+    state.items[i].checked = !state.items[i].checked;
+}
+
 ////
+
+// HTML for ITEM we're adding, when we click 'Add item'
+var itemTemplate = (
+    '<li>' +
+        '<span class="shopping-item">ITEM</span>' +
+        '<div class="shopping-item-controls">' +
+        '<button class="shopping-item-toggle">' +
+        '<span class="button-label">check</span>' +
+        '</button>' +
+        '<button class="shopping-item-delete">' +
+        '<span class="button-label">delete</span>' +
+        '</button>' +
+        '</div>' +
+        '</li>'
+);
 
 // Page update
 function renderList(state, itemTemplate, element) {
     var itemsHTML = state.items.map(function(item){
         var listItem = $(itemTemplate);
-        listItem.find('li').addClass(item);
-        listItem.find('.shopping-item').text(item);
+        listItem.find('.shopping-item').text(item.itemName);
+
+        var i = getItem(state, item.itemName);
+        if (state.items[i].checked) {
+            listItem.find('.shopping-item')
+                .addClass('shopping-item__checked');
+        }
         return listItem;
     });
     element.html(itemsHTML);
 };
 
+////
 
 // Event listeners
 function submitFormAddItem() {
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        addItem(state, $('#shopping-list-entry').val());
+        addItem(state, $('#shopping-list-entry').val().trim());
         renderList(state, itemTemplate, $('.shopping-list'));
-        $('#shopping-list-entry').val('');
-        getFocus();
+        getFormFocus();
     });
 }
 
 function deleteButton() {
     $('.shopping-list').on('click', '.shopping-item-delete', function(event){
-        var item = $(this).parents('li').find('.shopping-item').text();
-        removeItem(state, item);
+        var itemText = $(this).parents('li').find('.shopping-item').text();
+        removeItem(state, itemText);
         renderList(state, itemTemplate, $('.shopping-list'));
     });
 }
 
 function checkButton() {
     $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
-        $(this).parents('li').find('.shopping-item').toggleClass('shopping-item__checked');
+        var itemObj = $(this).parents('li').find('.shopping-item');
+        checkItem(state, itemObj.text());
+        // alert(itemObj.text());
+        itemObj.toggleClass('shopping-item__checked');
     });
 }
 
-function getFocus() {
+function getFormFocus() {
     $('input[name=shopping-list-entry]').focus();
+    $('#shopping-list-entry').val('');
 }
 
 $(document).ready(
@@ -97,5 +112,5 @@ $(document).ready(
         submitFormAddItem();
         deleteButton();
         checkButton();
-        getFocus();
+        getFormFocus();
 });
